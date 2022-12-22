66b63b548bba6c387ab544f0fe604d3481f3db3f
Erik Rogers <erik@underlogic.tech>
2022-12-22 00:45:29 -0500

    feat: add basic parsing of scalars

--- Runtime/TomlReader.cs
+++ Runtime/TomlReader.cs
@@ -1,13 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnderLogic.Serialization.Toml.Types;
 
 namespace UnderLogic.Serialization.Toml
 {
     internal class TomlReader : IDisposable
     {
-        private const string DateFormat = "yyyy-MM-dd HH:mm:ss.fffZ";
+        private static readonly Regex KeyValueRegex = new (@"^\s*([\w.-]+)\s*=\s*(.*)", RegexOptions.Compiled);
 
         private readonly TextReader _reader;
         private bool _isDisposed;
@@ -33,7 +35,125 @@ namespace UnderLogic.Serialization.Toml
 
         public TomlTable ReadDocument()
         {
-            return new TomlTable();
+            CheckIfDisposed();
+
+            var rootTable = new TomlTable();
+
+            var currentTable = rootTable;
+
+            var tableStack = new Stack<TomlTable>();
+            
+            string line = null;
+            while ((line = _reader.ReadLine()) != null)
+            {
+                // Skip comments
+                if (line.StartsWith("#"))
+                    continue;
+
+                // Parse key-value pairs
+                var keyValueMatch = KeyValueRegex.Match(line);
+                if (keyValueMatch.Success)
+                {
+                    var key = keyValueMatch.Groups[1].Value.Trim();
+                    var valueString = keyValueMatch.Groups[2].Value.Trim();
+
+                    if (TryParseScalarValue(valueString, out var scalarValue))
+                    {
+                        currentTable.Add(key, scalarValue);
+                        continue;
+                    }
+                }
+            }
+
+            return rootTable;
+        }
+
+        private static bool TryParseScalarValue(string text, out TomlValue tomlValue)
+        {
+            tomlValue = null;
+
+            if (text == "null")
+            {
+                tomlValue = TomlNull.Value;
+                return true;
+            }
+
+            if (TryParseBooleanValue(text, out var boolValue))
+                tomlValue = boolValue;
+            else if (TryParseStringValue(text, out var stringValue))
+                tomlValue = stringValue;
+            else if (TryParseFloatValue(text, out var floatValue))
+                tomlValue = floatValue;
+            else if (TryParseIntegerValue(text, out var integerValue))
+                tomlValue = integerValue;
+            else if (TryParseDateTimeValue(text, out var dateTimeValue))
+                tomlValue = dateTimeValue;
+
+            return tomlValue != null;
+        }
+
+        private static bool TryParseBooleanValue(string text, out TomlBoolean tomlValue)
+        {
+            tomlValue = null;
+
+            if (text != "true" && text != "false")
+                return false;
+            
+            tomlValue = new TomlBoolean(text == "true");
+            return true;
+        }
+
+        private static bool TryParseStringValue(string text, out TomlString tomlValue)
+        {
+            tomlValue = null;
+            
+            var wrappedInDoubleQuotes = text.StartsWith('"') && text.EndsWith('"');
+            var wrappedInSingleQuotes = text.StartsWith("'") && text.EndsWith("'");
+            
+            if (!wrappedInDoubleQuotes && !wrappedInSingleQuotes)
+                return false;
+            
+            tomlValue = new TomlString(text.Substring(1, text.Length - 2));
+            return true;
+        }
+        
+        private static bool TryParseFloatValue(string text, out TomlFloat tomlValue)
+        {
+            tomlValue = null;
+
+            var sanitizedText = text.Replace("_", "");
+            if (!sanitizedText.Contains('.') && !sanitizedText.Contains('e'))
+                return false;
+            
+            if (!double.TryParse(sanitizedText, out var doubleValue))
+                return false;
+            
+            tomlValue = new TomlFloat(doubleValue);
+            return true;
+        }
+        
+        private static bool TryParseIntegerValue(string text, out TomlInteger tomlValue)
+        {
+            tomlValue = null;
+
+            var sanitizedText = text.Replace("_", "");
+            
+            if (!long.TryParse(sanitizedText, out var int64Value))
+                return false;
+            
+            tomlValue = new TomlInteger(int64Value);
+            return true;
+        }
+        
+        private static bool TryParseDateTimeValue(string text, out TomlDateTime tomlValue)
+        {
+            tomlValue = null;
+
+            if (!DateTime.TryParse(text, out var dateTimeValue))
+                return false;
+            
+            tomlValue = new TomlDateTime(dateTimeValue);
+            return true;
         }
 
         public void Close()
